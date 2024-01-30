ea77fa0da11ca0f877dfd005705043f273286cc6
Gavin Palmer <gavin.palmer7@gmail.com>
2024-01-30 21:02:45 +0000

    added basic logic

--- HealthPartnersCodingTest/Interfaces/IFizzBuzzService.cs
+++ HealthPartnersCodingTest/Interfaces/IFizzBuzzService.cs
@@ -7,3 +7,4 @@ namespace HealthPartnersCodingTest.Interfaces
         string GetStringValue();
     }
 }
+    
--- HealthPartnersCodingTest/Program.cs
+++ HealthPartnersCodingTest/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using HealthPartnersCodingTest.Interfaces;
 
 namespace HealthPartnersCodingTest
 {
@@ -6,7 +9,31 @@ namespace HealthPartnersCodingTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var serviceTypes = System.Reflection.Assembly.GetExecutingAssembly().GetTypes()
+                .Where(mytype => mytype.GetInterfaces().Contains(typeof(IFizzBuzzService))).ToList();
+
+            var services = serviceTypes.Select(type => (IFizzBuzzService)Activator.CreateInstance(type)).ToList();
+
+            for (var i = 1; i <= 100; i++)
+            {
+                var value = string.Empty;
+                foreach (var service in services)
+                {
+                    if (service.IsValid(i))
+                    {
+                        value = value + service.GetStringValue();
+                    }
+                }
+                if (string.IsNullOrEmpty(value))
+                {
+                    Console.WriteLine(i);
+                }
+                else
+                {
+                    Console.WriteLine(value);
+                }
+            }
+
         }
     }
 }
